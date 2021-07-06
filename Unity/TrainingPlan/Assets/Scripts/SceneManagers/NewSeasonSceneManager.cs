eaef64c374b895a085b470f81294fb18772d42ab
Romero Victor <victor.romero@grenoble-inp.fr>
2021-07-06 19:17:10 +0200

    Create main scenes and import TMP asset

---
+++ Unity/TrainingPlan/Assets/Scripts/SceneManagers/NewSeasonSceneManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace Aperomero.TrainingPlan.SceneManagement
+{
+
+    public class NewSeasonSceneManager : MonoBehaviour
+    {
+
+        #region Variables
+
+        private string templatePath = "Assets/Resources/Seasons/SeasonTemplate.csv";
+        private string parametersPath = "Assets/Resources/Parameters/ParametersTemplate.csv";
+
+        #endregion
+
+        #region Unity Methods
+
+        private void Start()
+        {
+
+            if (File.Exists(templatePath) && File.Exists(parametersPath))
+            {
+                PrivateTools.TODO("Allow creation of new set of parameters");
+            }
+            else
+            {
+                Debug.LogError("Problems to found parameters and season templates");
+            }
+                        
+        }
+
+        #endregion
+
+    }
+
+}
---
+++ Unity/TrainingPlan/Assets/Scripts/SceneManagers/WelcomeSceneManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Aperomero.TrainingPlan.SceneManagement
+{
+
+    public class WelcomeSceneManager : MonoBehaviour
+    {
+
+        #region Buttons Reactions
+
+        public void CreateNewSeason()
+        {
+            SceneManager.LoadScene("NewSeasonScene");
+        }
+
+        #endregion
+
+    }
+
+}
